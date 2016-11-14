056f59da85268c11c2b499438765cd43712038ff
dhanyapai <dhanya@tplinkra.com>
2016-11-14 00:32:57 -0800

    Added Nested routes  to index.js and rendered these nested child components in App.js

--- lessons/04-nested-routes/modules/App.js
+++ lessons/04-nested-routes/modules/App.js
@@ -5,11 +5,12 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <h1>React Router Tutorial</h1>
+        <h1>React Router Experiments</h1>
         <ul role="nav">
           <li><Link to="/about">About</Link></li>
           <li><Link to="/repos">Repos</Link></li>
         </ul>
+          {this.props.children}
       </div>
     )
   }
