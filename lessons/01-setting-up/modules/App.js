47954bef8da2fcb1280447d384e796e5fbd566f5
dhanyapai <dhanya@tplinkra.com>
2016-11-13 23:47:07 -0800

    First commit with skelton framework in place

---
+++ lessons/01-setting-up/modules/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return <div>Hello I'm Dhanya, Experimenting with React Router!</div>
+  }
+})
---
+++ lessons/02-rendering-a-route/modules/Repos.js
@@ -0,0 +1,10 @@
+/**
+ * Created by dhanyapai on 11/13/16.
+ */
+import React from 'react'
+
+export default React.createClass({
+    render() {
+        return <div>Repos</div>
+    }
+})
